863b26ebd7a4bdb36a74f66efcf86dcfc5fd76a4
Ari Wilson <ari.wilson@gmail.com>
2009-12-19 21:56:40 -0800

    Actually measures accelerometer values! Not quite zeroed on x and y so I'm not sure what's happening. But it does something! And doesn't crash. There's a message-passing architecture now that I'm not sure is necessary and I have lots of overrides on the Activity that do nothing but call the superclass. But it works!

--- Seismo/src/com/arilwilson/seismo/AccelerometerReader.java
+++ Seismo/src/com/arilwilson/seismo/AccelerometerReader.java
@@ -5,22 +5,22 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class AccelerometerReader { 
-  public volatile float direction = 0;
-  public volatile float inclination;
-  public volatile float rollingZ = 0;
+  public volatile float x = 0;
+  public volatile float y = 0;
+  public volatile float z = 0;
 
-  public volatile float kFilteringFactor = (float)0.05;
-  public float aboveOrBelow = 0;
-	
   public AccelerometerReader(Context ctx) 
       throws UnsupportedOperationException { 
     SensorManager sensor_manager = (SensorManager) ctx.getSystemService(
         Context.SENSOR_SERVICE);
-    sensor_manager.registerListener(
-        listener_, sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-        SensorManager.SENSOR_DELAY_FASTEST); 
+    Sensor accelerometer = sensor_manager.getDefaultSensor(
+        Sensor.TYPE_ACCELEROMETER);
+    Log.i("Seismo", accelerometer.getName());
+    sensor_manager.registerListener(listener_, accelerometer,
+    		                        SensorManager.SENSOR_DELAY_FASTEST); 
   }
 
   private SensorEventListener listener_ = new SensorEventListener(){
@@ -29,24 +29,10 @@ public class AccelerometerReader {
     public void onSensorChanged(SensorEvent evt) {
       float vals[] = evt.values;
       
-      if(evt.sensor.getType() == Sensor.TYPE_ORIENTATION) {
-        float rawDirection = vals[0];
-
-        direction =(float) ((rawDirection * kFilteringFactor) + 
-            (direction * (1.0 - kFilteringFactor)));
-
-        inclination = 
-            (float) ((vals[2] * kFilteringFactor) + 
-            (inclination * (1.0 - kFilteringFactor)));
-
-                
-        if(aboveOrBelow > 0) inclination = inclination * -1;
-          
-        if(evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            aboveOrBelow =
-                (float) ((vals[2] * kFilteringFactor) + 
-                (aboveOrBelow * (1.0 - kFilteringFactor)));
-        }
+      if(evt.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        x = vals[0];
+        y = vals[1];
+        z = vals[2];
       }
     }
   };
--- Seismo/src/com/arilwilson/seismo/Seismo.java
+++ Seismo/src/com/arilwilson/seismo/Seismo.java
@@ -4,24 +4,39 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.widget.TextView;
 
 public class Seismo extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	super.onCreate(savedInstanceState);
     AccelerometerReader reader = new AccelerometerReader(this);
-    direction_view_ = new TextView(this);
+    view_ = new TextView(this);
     updater_ = new AccelerometerUpdater(reader, ui_updater_);
-    setContentView(direction_view_);
+    setContentView(view_);
     updater_thread_ = new Thread(updater_);
     updater_thread_.start();
   }
 
   @Override
+  public void onPause() {
+    super.onPause();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+  }
+
+  @Override
   public void onDestroy() {
-    updater_.destroy();
+    super.onDestroy();
+	updater_.destroy();
     try {
       updater_thread_.join();
     } catch (InterruptedException e) {
@@ -42,7 +57,9 @@ public class Seismo extends Activity {
         Message m = new Message();
         m.setData(b);
         b.putString("action", "update");
-        b.putDouble("direction", reader_.direction);
+        b.putDouble("x", reader_.x);
+        b.putDouble("y", reader_.y);
+        b.putDouble("z", reader_.z);
         ui_updater_.sendMessage(m);
         try {
             Thread.sleep(1000, 0);
@@ -67,13 +84,15 @@ public class Seismo extends Activity {
       Bundle b = m.getData();
       if (b != null) {
         if (b.getString("action") == "update") {
-          direction_view_.setText(String.valueOf(b.getDouble("direction")));
+          view_.setText(String.valueOf(b.getDouble("x")) + " " +
+        		        String.valueOf(b.getDouble("y")) + " " +
+        		        String.valueOf(b.getDouble("z")));
         }
       }
     }
   };
 
-  private TextView direction_view_;
+  private TextView view_;
   private AccelerometerUpdater updater_;
   private Thread updater_thread_;
 }
