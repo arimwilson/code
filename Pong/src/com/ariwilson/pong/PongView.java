64fe14878da6336636c59d021ad9928ad873d6aa
Ari Wilson <ari.wilson@gmail.com>
2010-01-11 19:57:16 -0800

    random

--- Pong/src/com/ariwilson/pong/PongView.java
+++ Pong/src/com/ariwilson/pong/PongView.java
@@ -1,7 +1,13 @@
 package com.ariwilson.pong;
 
+import android.content.Context;
 import android.view.View;
 
 public class PongView extends View {
+  public PongView(Context ctx) {
+    super(ctx);
+    ctx_ = ctx;
+  }
 
+  private Context ctx_;
 }
