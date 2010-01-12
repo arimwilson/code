beb219a268b9002aac04c6838fd2e7020ceac2cb
Ari Wilson <ari.wilson@gmail.com>
2010-01-12 02:04:47 -0800

    Still good as an game template. Basic version of Pong drawing a white screen.

--- Pong/src/com/ariwilson/pong/Pong.java
+++ Pong/src/com/ariwilson/pong/Pong.java
@@ -3,6 +3,7 @@ package com.ariwilson.pong;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
@@ -10,11 +11,13 @@ public class Pong extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                          WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     FrameLayout layout = new FrameLayout(this);
     view_ = new PongView(this);
+    layout.addView(view_);
     setContentView(layout);
   }
 
--- Pong/src/com/ariwilson/pong/PongView.java
+++ Pong/src/com/ariwilson/pong/PongView.java
@@ -1,13 +1,57 @@
 package com.ariwilson.pong;
 
 import android.content.Context;
-import android.view.View;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class PongView extends View {
+public class PongView extends SurfaceView implements SurfaceHolder.Callback {
   public PongView(Context ctx) {
     super(ctx);
+    holder_ = getHolder();
+    holder_.addCallback(this);
+    setKeepScreenOn(true);
     ctx_ = ctx;
+  } 
+
+  public void surfaceChanged(SurfaceHolder holder, int format, int width,
+      int height) {
+    width_ = width;
+    height_ = height;
   }
 
+  public void surfaceCreated(SurfaceHolder holder) {
+    running_ = true;
+    thread_ = new PongViewThread();
+    thread_.start();
+  }
+
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    running_ = false;
+    try {
+      thread_.join();
+    } catch (InterruptedException e) {
+      // Do nothing.
+    }
+  }
+
+  private class PongViewThread extends Thread {
+    @Override
+    public void run() {
+      while (running_) {
+        synchronized (holder_) {
+          Canvas canvas = holder_.lockCanvas();
+          canvas.drawARGB(255, 255, 255, 255);
+          holder_.unlockCanvasAndPost(canvas);
+        }
+      }
+    }
+  }
+
+  private boolean running_ = false;
+  private int width_;
+  private int height_;
+  private PongViewThread thread_;
+  private SurfaceHolder holder_;
   private Context ctx_;
 }
