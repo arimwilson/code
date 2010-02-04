1e6800ca7533199d00e5209465bb4ab2078d0352
Ari Wilson <ari.wilson@gmail.com>
2010-02-03 16:22:04 -0800

    Couple of things: 1. Added stubs for submit / list high score methods for AppEngine. 2. Split out update/draw threads for Pong and capped FPS at 30. More basic game    setup should be added from wiki. Still a good template.

--- AndroidScores/main.py
+++ AndroidScores/main.py
@@ -2,13 +2,26 @@ import wsgiref.handlers
 
 from google.appengine.ext import webapp
 
-class MainHandler(webapp.RequestHandler):
+class SubmitHandler(webapp.RequestHandler):
   def get(self):
     pass
 
+  def post(self):
+    pass
+
+
+class ListHandler(webapp.RequestHandler):
+  def get(self):
+    pass
+
+  def post(self):
+    pass
+
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
+  application = webapp.WSGIApplication([
+      ('/submit', SubmitHandler),
+      ('/list', ListHandler),
+    ], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == '__main__':
--- Pong/src/com/ariwilson/pong/PongView.java
+++ Pong/src/com/ariwilson/pong/PongView.java
@@ -22,36 +22,63 @@ public class PongView extends SurfaceView implements SurfaceHolder.Callback {
 
   public void surfaceCreated(SurfaceHolder holder) {
     running_ = true;
-    thread_ = new PongViewThread();
-    thread_.start();
+    draw_thread_ = new DrawThread();
+    update_thread_ = new UpdateThread();
+    draw_thread_.start();
+    update_thread_.start();
   }
 
   public void surfaceDestroyed(SurfaceHolder holder) {
     running_ = false;
     try {
-      thread_.join();
+      draw_thread_.join();
     } catch (InterruptedException e) {
       // Do nothing.
     }
   }
 
-  private class PongViewThread extends Thread {
+  private class DrawThread extends Thread {
     @Override
     public void run() {
       while (running_) {
+        try {
+          ctx_.wait(UPDATE_MS * 2);
+        } catch (InterruptedException e) {
+          // Do nothing.
+        }
         synchronized (holder_) {
           Canvas canvas = holder_.lockCanvas();
           canvas.drawARGB(255, 255, 255, 255);
+          // TODO(ariw): Draw all objects.
           holder_.unlockCanvasAndPost(canvas);
         }
       }
     }
   }
 
+  private class UpdateThread extends Thread {
+    @Override
+    public void run() {
+      while (running_) {
+        long start_time = System.currentTimeMillis();
+        // TODO(ariw): Update all objects.
+        ctx_.notify();
+        try {
+          sleep(UPDATE_MS - System.currentTimeMillis() + start_time);
+        } catch (InterruptedException e) {
+          // Do nothing.
+        }
+      }
+    }
+  }
+
+  public static final int UPDATE_MS = 33;  // ~30 updates/s  
+
   private boolean running_ = false;
   private int width_;
   private int height_;
-  private PongViewThread thread_;
+  private DrawThread draw_thread_;
+  private UpdateThread update_thread_;
   private SurfaceHolder holder_;
   private Context ctx_;
 }
