b69ce7209b5e42bef254e42bace4fedb5876c577
Ari Wilson <ari.wilson@gmail.com>
2011-01-09 18:48:04 -0800

    Moved to using only one HTML file + jQuery for ease of use for web app. Primarily done so that password will be persisted thru server call. Next will add actual server call.

--- KeeStand/keestand.py
+++ KeeStand/keestand.py
@@ -1,23 +1,39 @@
-import wsgiref.handlers
-
+from google.appengine.ext import blobstore
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class User(db.Model):
+  username = db.StringProperty(required = True)
+  password_hash = db.StringProperty(required = True)
+  file_key = blobstore.BlobReferenceProperty()
 
 class LoginHandler(webapp.RequestHandler):
   def post(self):
     username = self.request.get("username")
-    password = self.request.get("password")
-    self.response.out.write("<html><title>KeeStand</title><body>%s %s</body></html>" % (username, password))
+    password_hash = self.request.get("password")
+    query = User.all()
+    query.filter("username =", username)
+    user = query.get()
+    if user and password_hash <> user.password_hash:
+      pass
+    elif not user:
+      user = User()
+      user.username = username
+      user.password_hash = password_hash
+      user.put()
+    self.response.headers["Set-Cookie"] = "username:%s" % username
 
-class EditHandler(webapp.RequestHandler):
+class SaveHandler(webapp.RequestHandler):
   def post(self):
     pass
 
 def main():
   application = webapp.WSGIApplication([
       ('/script/login', LoginHandler),
-      ('/script/edit', EditHandler),
+      ('/script/save', SaveHandler),
     ])
-  wsgiref.handlers.CGIHandler().run(application)
+  run_wsgi_app(application)
 
 if __name__ == '__main__':
   main()
