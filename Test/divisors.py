128d46aee029c0f98b30a0db2db9349cfe40cda2
Ari Wilson <ari.wilson@gmail.com>
2011-03-27 23:53:27 -0700

    divisors

---
+++ Test/divisors.py
@@ -0,0 +1,16 @@
+# Find numbers with the most divisors in some range.
+
+import math
+
+num_divisors = []
+for i in range(2, 1001):
+  k = 0
+  l = []
+  for j in range(2, i):
+    if i % j == 0:
+      l.append(j)
+      k += 1
+  num_divisors.append((i, k, l))
+
+print sorted(num_divisors, reverse=True, key=lambda (i, k, l): k)[:5]
+
