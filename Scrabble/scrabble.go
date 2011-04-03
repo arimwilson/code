e88377892f15beeb1cdc6f9325f224307547b081
Ari Wilson <ari.wilson@gmail.com>
2011-04-03 02:26:37 -0700

    it for tonight

--- Scrabble/scrabble.go
+++ Scrabble/scrabble.go
@@ -1,3 +1,15 @@
-package main
+package scrabble
 
-import ("fmt"; "os";)
+import ("flag"; "fmt"; "os";)
+
+var wordList = flag.String("w", "",
+                           "File with space-separated list of legal words.")
+var board = flag.String("b", "", "File with board structure.")
+
+func main() {
+  flag.Parse()
+  wordListFile = os.Open(wordList, os.O_READONLY, 0);
+  for i := 0; 
+  boardFile = os.Open(board, os.O_READONLY, 0);
+  os.Stdin
+}
