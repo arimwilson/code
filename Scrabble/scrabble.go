65b0f5fb22730e9be08223c78a731b07ac676c2a
Ari Wilson <ari.wilson@gmail.com>
2011-04-03 18:47:18 -0700

    start reading board file

--- Scrabble/scrabble.go
+++ Scrabble/scrabble.go
@@ -1,15 +1,44 @@
-package scrabble
+// Scrabble move generator. Given a word list, board, and your current tiles,
+// outputs all legal moves ranked by point value.
 
-import ("flag"; "fmt"; "os";)
+package main
+
+import ("bufio"; "flag"; "fmt"; "os";)
 
 var wordList = flag.String("w", "",
                            "File with space-separated list of legal words.")
 var board = flag.String("b", "", "File with board structure.")
+var tiles = flag.String("t", "", "Comma-separated list of player tiles.")
+
+func readWordList(wordListFile* os.File) {
+  wordListReader := bufio.NewReader(wordListFile)
+  for {
+    word, err := wordListReader.ReadString(" "[0])
+    if err != nil {
+      return
+    }
+    fmt.Printf(word + "\n")
+  }
+}
+
+func readBoard(boardFile* os.File) {
+  
+}
 
 func main() {
   flag.Parse()
-  wordListFile = os.Open(wordList, os.O_READONLY, 0);
-  for i := 0; 
-  boardFile = os.Open(board, os.O_READONLY, 0);
-  os.Stdin
+  wordListFile, err := os.Open(*wordList, os.O_RDONLY, 0);
+  defer wordListFile.Close();
+  if err != nil {
+    fmt.Printf("need valid file for -w, found %s", *wordList)
+    os.Exit(1)
+  }
+  readWordList(wordListFile)
+  boardFile, err := os.Open(*board, os.O_RDONLY, 0);
+  defer boardFile.Close();
+  if err != nil {
+    fmt.Printf("need valid file for -b, found %s", *board)
+    os.Exit(1)
+  }
+  readBoard(boardFile)
 }
