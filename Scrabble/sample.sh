da452970de7d2a22213569ae0e8af0e5988bc1dd
Ari Wilson <ari.wilson@gmail.com>
2011-04-23 21:53:01 -0700

    got rid of google claims

--- Scrabble/sample.sh
+++ Scrabble/sample.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-# Copyright 2011 Google Inc. All Rights Reserved.
-# Author: ariw@google.com (Ari Wilson)
 
 6g trie.go && 6g moves.go && 6g sortwith.go && 6g scrabble.go && 6l scrabble.6 && ./6.out -w twl.txt -b sample.txt -t "saedbqh"
+rm 6.out trie.6 moves.6 sortwith.6 scrabble.6
