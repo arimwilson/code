31075fdbd8a295b63f3fa388ce678160188613d3
Ari Wilson <ari.wilson@gmail.com>
2011-05-08 21:37:13 -0700

    various stuff

--- Scrabble/sample.sh
+++ Scrabble/sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-6g trie.go && 6g moves.go && 6g sortwith.go &&6g scrabble.go && 6l scrabble.6 && ./6.out -w twl.txt -b sample.txt -t "saedbqh"
+6g moves.go && 6g sortwith.go && 6g trie.go && 6g util.go && 6g scrabble.go && 6l scrabble.6 && ./6.out -w twl.txt -b sample.txt -t "saedbqh"
 rm 6.out trie.6 moves.6 sortwith.6 scrabble.6
