2f750e6f24bd4c8362bf3f26a415cfe541e9f1a1
Ari Wilson <ari.wilson@gmail.com>
2011-10-31 01:43:58 -0700

    Switching interesaint back to Go - could get processing intensive and want to learn Go. Get started with adding feeds / updating them.

---
+++ Interesaint/interesaint/interesaint.go
@@ -0,0 +1,55 @@
+// Find new interesting items from an RSS feed.
+
+import ("appengine"; "appengine/datastore"; "appengine/urlfetch"; "http")
+
+func init() {
+  http.HandleFunc("/add", add)
+  http.HandleFunc("/tasks/update", update)
+}
+
+type Feed struct {
+  Url string
+}
+
+type Subscription struct {
+  User string
+  FeedId *datastore.Key
+}
+
+type Item struct {
+  Content string
+  FeedId *datastore.Key
+}
+
+type Rating struct {
+  UserId *datastore.Key
+  ItemId *datastore.Key
+  interesting bool
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+  c := appengine.NewContext(r)
+  err := r.ParseForm()
+  if err != nil {
+    c.Errorf("Could not parse form with error: %s", err.String())
+    http.Error(w, err.String(), http.StatusInternalServerError)
+    return
+  }
+
+  url := r.FormValue("url")
+  query := datastore.NewQuery("feed")
+  query.Filter("Url =", url)
+  feed := new(Feed)
+  query.Run(c).Next(feed)
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+  c := appengine.NewContext(r)
+  err := r.ParseForm()
+  if err != nil {
+    c.Errorf("Could not parse form with error: %s", err.String())
+    http.Error(w, err.String(), http.StatusInternalServerError)
+    return
+  }
+}
+
