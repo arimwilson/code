b890d7d39829a4590cbbc696b6c13ccd554ec887
Ari Wilson <ari.wilson@gmail.com>
2012-01-02 20:26:19 -0800

    futzing around with least squares... need to stabilize

--- Test/learn.py
+++ Test/learn.py
@@ -1,24 +1,38 @@
 import numpy
 
-f = open("ratings.txt")
-features = [s.replace("\"", "").replace("\n", "").split(",") for s in f.readlines()]
-features = [[float(s[0]),] + s[1:] for s in features]
-features = [s[:-1] + [t for t in s[-1].split(" ") if t != ""] for s in features]
-counts = {}
-scores = [s[0] for s in features]
-features = [s[1:] for s in features]
-allfeatures = set()
-print features[0]
+def break_row(line):
+  line = line.split(",", 1)
+  return float(line[0]), feature_row(line[1])
+
+def feature_row(line):
+  line = line.replace("\"", "").replace("\n", "").split(",")
+  line = line[:-1] + [word for word in line[-1].split(" ") if word != ""]
+  return line
+
+def coeff_row(features, all_features):
+  coeff = []
+  for feature in all_features:
+    coeff.append(features.count(feature))
+  return coeff
+
+def scoring(line, all_features, x):
+  return (numpy.array(coeff_row(feature_row(line), all_features)) * x).sum()
+
+lines = open("ratings.txt").readlines()
+scores =[]
+features = []
+for line in lines:
+  score, feature = break_row(line)
+  scores.append(score)
+  features.append(feature)
+all_features = set()
 for s in features:
   for t in s:
-    allfeatures.add(t)
-allfeatures_ordered = list(allfeatures)
-coeffs = []
-for s in features:
-  coeff = []
-  for feature in allfeatures_ordered:
-    coeff.append(s.count(feature))
-  coeffs.append(coeff)
-print allfeatures_ordered
-print coeffs[0]
-print scores[0]
+    all_features.add(t)
+all_features_ordered = list(all_features)
+coeffs = [coeff_row(feature, all_features_ordered) for feature in features]
+coeffs = numpy.array(coeffs)
+scores = numpy.array(scores)
+x, residues, rank, s = numpy.linalg.lstsq(coeffs, scores)
+test = "\"ari.wilson\",\"mmmm.hm_-_tv_central_forum\",\"file  project accessory s01e05 beach blanket blingo ws dsr xvid  ny2  avi thread  project accessory season 1\""
+print scoring(test, all_features_ordered, x)
