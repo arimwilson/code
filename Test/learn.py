03887f1e375eaff1dc4988768e25c45ce904be40
Ari Wilson <ari.wilson@gmail.com>
2012-01-03 00:55:37 -0800

    feature scaling

--- Test/learn.py
+++ Test/learn.py
@@ -10,16 +10,40 @@ def feature_row(line):
   return line
 
 def coeff_row(features, all_features):
-  coeff = []
+  coeff = [1]  # y intercept
   for feature in all_features:
     coeff.append(features.count(feature))
   return coeff
 
-def scoring(line, all_features, x):
-  return (numpy.array(coeff_row(feature_row(line), all_features)) * x).sum()
+def feature_scale(coeffs):
+  mins = [1000] * (len(coeffs[0]) - 1)
+  maxes = [-1000] * (len(coeffs[0]) - 1)
+  sums = [0] * (len(coeffs[0]) - 1)
+  for coeff in coeffs:
+    for i in range(len(coeff) - 1):
+      sums[i] += coeff[i + 1]
+      if coeff[i + 1] < mins[i]:
+        mins[i] = coeff[i + 1]
+      if coeff[i + 1] > maxes[i]:
+        maxes[i] = coeff[i + 1]
+  means = [float(sums[i]) / len(coeffs) for i in range(len(sums))]
+  ranges = [maxes[i] - mins[i] for i in range(len(maxes))]
+  return means, ranges
+
+def scale(coeff, means, ranges):
+  scaled = [coeff[0]]
+  for i in range(len(coeff) - 1):
+    if ranges[i] > 0:
+      scaled.append(float(coeff[i + 1] - means[i]) / ranges[i])
+    else:
+      scaled.append(0)
+  return scaled
+
+def scoring(line, all_features, mins, maxes, x):
+  return (numpy.array(scale(coeff_row(feature_row(line), all_features), mins, maxes)) * x).sum()
 
 lines = open("ratings.txt").readlines()
-scores =[]
+scores = []
 features = []
 for line in lines:
   score, feature = break_row(line)
@@ -31,8 +55,10 @@ for s in features:
     all_features.add(t)
 all_features_ordered = list(all_features)
 coeffs = [coeff_row(feature, all_features_ordered) for feature in features]
-coeffs = numpy.array(coeffs)
+means, ranges = feature_scale(coeffs)
+coeffs = numpy.array([scale(coeff, means, ranges) for coeff in coeffs])
 scores = numpy.array(scores)
 x, residues, rank, s = numpy.linalg.lstsq(coeffs, scores)
 test = "\"ari.wilson\",\"mmmm.hm_-_tv_central_forum\",\"file  project accessory s01e05 beach blanket blingo ws dsr xvid  ny2  avi thread  project accessory season 1\""
-print scoring(test, all_features_ordered, x)
+print scoring(test, all_features_ordered, means, ranges, x)
+print x
