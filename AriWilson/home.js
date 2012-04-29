b3f1518932328e4321f0f700779bbe36b89db609
Ari Wilson <ari.wilson@gmail.com>
2012-04-29 01:38:02 -0700

    adding some interactivity

--- AriWilson/home.js
+++ AriWilson/home.js
@@ -1,30 +1,83 @@
-function start() {
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(1024, 768);
-  document.body.appendChild(renderer.domElement);
+var HOME = {
+}
 
-  var scene = new THREE.Scene();
+HOME.keydown(event) {
+  if (event.keyCode == 37) {  // left
+    HOME.horizontalSpeed -= 1;
+    return false;
+  } else if (event.keyCode == 38) {  // up
+    HOME.forwardSpeed += 1;
+    return false;
+  } else if (event.keyCode == 39) {  // right
+    HOME.horizontalSpeed += 1;
+    return false;
+  } else if (event.keyCode == 40) { // down
+    HOME.forwardSpeed -= 1;
+    return false;
+  }
+}
 
-  var camera = new THREE.PerspectiveCamera(
-    35,             // Field of view
-    800 / 600,      // Aspect ratio
-    0.1,            // Near plane
-    10000           // Far plane
+HOME.keyup(event) {
+  if (event.keyCode == 37) {  // left
+    HOME.horizontalSpeed += 1;
+    return false;
+  } else if (event.keyCode == 38) {  // up
+    HOME.forwardSpeed -= 1;
+    return false;
+  } else if (event.keyCode == 39) {  // right
+    HOME.horizontalSpeed -= 1;
+    return false;
+  } else if (event.keyCode == 40) { // down
+    HOME.forwardSpeed += 1;
+    return false;
+  }
+}
+
+HOME.mousemove(event) {
+}
+
+HOME.animate = function() {
+  // TODO(ariw): Update HOME.camera.position.
+  HOME.renderer.render(HOME.scene, HOME.camera);
+  window.webkitRequestAnimationFrame(HOME.animate);
+}
+
+HOME.start = function() {
+  HOME.renderer = new THREE.WebGLRenderer();
+  HOME.renderer.setSize(document.body.clientWidth, document.body.clientHeight);
+  document.body.appendChild(HOME.renderer.domElement);
+
+  // Set up the initial scene.
+  HOME.scene = new THREE.Scene();
+
+  HOME.camera = new THREE.PerspectiveCamera(
+    35,  // Field of view
+    document.body.clientWidth / document.body.clientHeight,  // Aspect ratio
+    0.1,  // Near plane
+    10000  // Far plane
   );
-  camera.position.set(-15, 10, 10);
-  camera.lookAt(scene.position);
+  HOME.camera.position.set(0, 0, 30);
+  HOME.camera.lookAt(scene.position);
 
-  scene.add(camera);
+  HOME.scene.add(camera);
 
   var cube = new THREE.Mesh(
     new THREE.CubeGeometry(5, 5, 5),
     new THREE.MeshLambertMaterial({ color: 0xFF0000 })
   );
-  scene.add(cube);
+  HOME.scene.add(cube);
 
   var light = new THREE.PointLight(0xFFFF00);
   light.position.set(10, 0, 10);
-  scene.add(light);
+  HOME.scene.add(light);
 
-  renderer.render(scene, camera);
+  // Keys ready to press.
+  HOME.horizontalSpeed = 0;
+  HOME.forwardSpeed = 0;
+  $("document").keydown(HOME.keydown);
+  $("document").keyup(HOME.keyup);
+  $("document").mousemove(HOME.mousemove);
+
+  HOME.animate();
 }
+
