606e8fc648a7a9e28d8ae7ebd6762f3777a01ff4
Ari Wilson <ari.wilson@gmail.com>
2012-04-29 13:56:33 -0700

    updates

--- AriWilson/home.js
+++ AriWilson/home.js
@@ -1,7 +1,7 @@
 var HOME = {
 }
 
-HOME.keydown(event) {
+HOME.keydown = function(event) {
   if (event.keyCode == 37) {  // left
     HOME.horizontalSpeed -= 1;
     return false;
@@ -17,7 +17,7 @@ HOME.keydown(event) {
   }
 }
 
-HOME.keyup(event) {
+HOME.keyup = function(event) {
   if (event.keyCode == 37) {  // left
     HOME.horizontalSpeed += 1;
     return false;
@@ -33,18 +33,53 @@ HOME.keyup(event) {
   }
 }
 
-HOME.mousemove(event) {
+HOME.mousemove = function(event) {
+  if (!HOME.pageX || !HOME.pageY) {
+    HOME.pageX = event.pageX;
+    HOME.pageY = event.pageY;
+    return;
+  }
+  // TODO(ariw): Figure out real formula here.
+  HOME.horizontalRotate += 10 * (event.pageX - HOME.pageX) / HOME.width;
+  HOME.verticalRotate += 10 * (event.pageY - HOME.pageY) / HOME.height;
 }
 
 HOME.animate = function() {
+  // See how much time has passed since the last frame.
+  var time = Date.now();
+  if (!HOME.oldTime) {
+    HOME.oldTime = time;
+    return;
+  }
+  var timeDiff = time - HOME.oldTime;
+  HOME.oldTime = time;
+
+  // Adjust where we're looking based on the mouse.
+  var look = HOME.camera.look;
+
+  look.x = Math.cos(Math.atan2(look.x, look.y) +
+                    HOME.horizontalRotate * 2 * Math.Pi / 360);
+  look.y = Math.sin(Math.atan2(look.x, look.y) +
+                    HOME.horizontalRotate * 2 * Math.Pi / 360);
+  look.z = ;
+  HOME.camera.lookAt(look);
+  HOME.horizontalRotate = 0;
+  HOME.verticalRotate = 0;
+
+  // Adjust where we are based on the keyboard.
+  var position = HOME.camera.position;
   // TODO(ariw): Update HOME.camera.position.
+
+  // Render the scene.
   HOME.renderer.render(HOME.scene, HOME.camera);
   window.webkitRequestAnimationFrame(HOME.animate);
 }
 
 HOME.start = function() {
   HOME.renderer = new THREE.WebGLRenderer();
-  HOME.renderer.setSize(document.body.clientWidth, document.body.clientHeight);
+  HOME.width = document.body.clientWidth;
+  HOME.height = document.body.clientHeight;
+  HOME.renderer.setSize(HOME.width, HOME.height);
   document.body.appendChild(HOME.renderer.domElement);
 
   // Set up the initial scene.
@@ -52,14 +87,14 @@ HOME.start = function() {
 
   HOME.camera = new THREE.PerspectiveCamera(
     35,  // Field of view
-    document.body.clientWidth / document.body.clientHeight,  // Aspect ratio
+    HOME.width / HOME.height,  // Aspect ratio
     0.1,  // Near plane
     10000  // Far plane
   );
   HOME.camera.position.set(0, 0, 30);
-  HOME.camera.lookAt(scene.position);
+  HOME.camera.look = HOME.scene.position.clone().normalize();
 
-  HOME.scene.add(camera);
+  HOME.scene.add(HOME.camera);
 
   var cube = new THREE.Mesh(
     new THREE.CubeGeometry(5, 5, 5),
@@ -71,13 +106,13 @@ HOME.start = function() {
   light.position.set(10, 0, 10);
   HOME.scene.add(light);
 
+  HOME.animate();
+
   // Keys ready to press.
   HOME.horizontalSpeed = 0;
   HOME.forwardSpeed = 0;
   $("document").keydown(HOME.keydown);
   $("document").keyup(HOME.keyup);
   $("document").mousemove(HOME.mousemove);
-
-  HOME.animate();
 }
 
