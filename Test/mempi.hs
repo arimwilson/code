d80dba7448d3d381e1b3ea9e321dad159da78bb2
Ari Wilson <ari.wilson@gmail.com>
2013-02-11 17:45:37 -0800

    More generic 'nextDigits' function.

--- Test/mempi.hs
+++ Test/mempi.hs
@@ -14,18 +14,19 @@ main = do
 
 memorizeNumber :: String -> String -> Int -> String
 memorizeNumber (x:xs) (y:ys) digits
-    | x /= y =
+    | x == y = memorizeNumber xs ys (digits+1)
+    | otherwise =
         printf "Wrong on digit %d. You typed %c but it should have been %c."
-        digits y x
-    | otherwise = memorizeNumber xs ys (digits + 1)
-memorizeNumber [] _ digits = "Correct for all known digits."
+               digits y x
 memorizeNumber ys [] digits =
-    let (remaining, count) = nextDigits ys 5 in
-        printf "Correct for %d digits. Next %d digits are %s." (digits - 1)
-               count remaining
+    let next_digits = firstN ys 5 in
+        printf "Correct for %d digits. Next %d digits are %s." (digits-1)
+               (length next_digits) next_digits
+memorizeNumber [] _ digits = "Correct for all known digits."
 
-nextDigits :: String -> Int -> (String, Int)
-nextDigits (x:xs) digits
-    | digits == 0 = ([], 0)
-    | otherwise = let (y, z) = (nextDigits xs (digits - 1)) in (x : y, z + 1)
-nextDigits [] digits = ([], 0)
+-- Get first n elements from a list.
+firstN :: [a] -> Int -> [a]
+firstN (x:xs) n
+    | n > 0 = (x : (firstN xs (n-1)))
+    | otherwise = []
+firstN [] n = []
