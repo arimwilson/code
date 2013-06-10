1be392817431c07f75397bb82e40c274123ce086
Ari Wilson <ari.wilson@gmail.com>
2013-06-09 18:55:07 -0700

    Check in Pebble example sports app

---
+++ MyTracksForPebble/src/pebblemytracks/MyTracksReceiver.java
@@ -0,0 +1,16 @@
+package pebblemytracks;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyTracksReceiver extends BroadcastReceiver {
+
+  // start my tracks pebble service
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Intent service = new Intent(context,PebbleSportsService.class);
+    context.startService(service);        
+  }
+}
+
