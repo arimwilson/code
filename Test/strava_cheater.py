364763079327e90eb772bebc48e427ef762f81eb
Ari Wilson <ari.wilson@gmail.com>
2013-08-29 14:49:32 -0700

    done

--- Test/strava_cheater.py
+++ Test/strava_cheater.py
@@ -13,10 +13,11 @@
 
 import math, sys
 
-GPX_POINT = """<trkpt lat="%(latitude)f" lon="%(longitude)">
+GPX_POINT = """<trkpt lat="%(latitude)f" lon="%(longitude)f">
 <ele>%(elevation)f</ele>
 <time>%(time)s</time>
-</trkpt>"""
+</trkpt>
+"""
 
 GPX_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 <gpx
@@ -36,8 +37,7 @@ xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/
 <type><![CDATA[cycling]]></type>
 <extensions><topografix:color>c0c0c0</topografix:color></extensions>
 <trkseg>
-%s
-</trkseg>
+%s</trkseg>
 </trk>
 </gpx>"""
 
@@ -51,38 +51,43 @@ EARTH_RADIUS_MI = 3963.19
 # projection (an approximation that works on small distances).
 def pythag_distance(start, end):
   # Convert to radians.
-  start = (start[0] * math.pi / 180, start[1] * math.pi / 180)
-  end = (end[0] * math.pi / 180, end[1] * math.pi / 180)
+  start = (math.radians(start[0]), math.radians(start[1]))
+  end = (math.radians(end[0]), math.radians(end[1]))
 
   # Convert to equirectangular projection, get Pythagorean distance.
   a = (end[1] - start[1]) * math.cos((start[0] + end[0]) / 2)
   b = (end[0] - start[0])
   return math.sqrt(a * a + b * b)
 
+def subtract_coords(start, end):
+  return (end[0] - start[0], end[1] - start[1])
+
 if __name__ == "__main__":
   assert len(sys.argv) == 7
 
   start = (float(sys.argv[1]), float(sys.argv[2]))
   end = (float(sys.argv[3]), float(sys.argv[4]))
+  speed_mi_h = float(sys.argv[5])
 
   distance = pythag_distance(start, end) * EARTH_RADIUS_MI
+  # TODO(ariw): This is super hacky.
+  distance_vect = subtract_coords(start, end)
   # Compute how many seconds we need in order to achieve target speed.
-  speed_mi_h = float(sys.argv[5])
   speed_mi_s = speed_mi_h / (60 * 60)
-  seconds_needed = distance / speed_mi_s
-
-  # Convert start/end to some 2D projection, Pythagoras to get distance, invert to get
-  # semi-evenly spaced points at desired speed between the two points.
-  gpx_file = open(sys.argv[6], "w")
+  seconds_needed = math.ceil(distance / speed_mi_s)
   points_gpx = ""
   elevation = GPX_HEIGHT_M
-  for i in range(0, seconds_needed):
+  for i in range(0, int(seconds_needed)):
     if i < 10:
       seconds = "0" + str(i)
     else:
       seconds = str(i)
     time = GPX_TIME % (seconds)
-    latitude = start + i *
+    latitude = start[0] + i * distance_vect[0] / seconds_needed
+    longitude = start[1] + i * distance_vect[1] / seconds_needed
     points_gpx += GPX_POINT % (locals())
+
+  gpx_file = open(sys.argv[6], "w")
   gpx_file.write(GPX_TEMPLATE % points_gpx)
+  gpx_file.close()
 
