e2e613939bc396b2f28065a80717101bc51e47ae
Ari Wilson <ari.wilson@gmail.com>
2013-11-18 00:16:58 -0800

    random

--- Falldown/src/js/pebble-js-app.js
+++ Falldown/src/js/pebble-js-app.js
@@ -1,4 +1,9 @@
 Pebble.addEventListener("appmessage",
     function(e) {
+      var req = new XMLHttpRequest();
+      req.open('POST', e.payload.url, true);
+      req.onload = function(e) {
+      }
+      req.send(null);
     }
 );
