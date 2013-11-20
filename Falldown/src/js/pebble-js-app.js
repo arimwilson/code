a622f994583ba3eef66f9d58fbf92e71a543a03e
Ari Wilson <ari.wilson@gmail.com>
2013-11-19 21:40:28 -0800

    ugh

--- Falldown/src/js/pebble-js-app.js
+++
@@ -1,9 +0,0 @@
-Pebble.addEventListener("appmessage",
-    function(e) {
-      var req = new XMLHttpRequest();
-      req.open('POST', e.payload.url, true);
-      req.onload = function(e) {
-      }
-      req.send(null);
-    }
-);
---
+++ Falldown/src/mac_key.h
@@ -0,0 +1,7 @@
+#ifndef MAC_KEY_H
+#define MAC_KEY_H
+
+const char* kMacKey = "To be or not to be, that is the question!";
+const int kMacKeyLength = 42;
+
+#endif
