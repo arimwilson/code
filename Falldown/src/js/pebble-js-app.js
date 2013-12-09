b38f8cfcd7763af23f2b6b7660abb87f837f7919
Ari Wilson <ari.wilson@gmail.com>
2013-12-08 21:53:53 -0800

    moar

--- Falldown/src/js/pebble-js-app.js
+++ Falldown/src/js/pebble-js-app.js
@@ -1,6 +1,10 @@
 Pebble.addEventListener("appmessage",
     function(e) {
       // Check to see if we're proxying a nonce or a score.
+      var method = e.payload.url.substr(e.payload.url.lastIndexOf("/") + 1);
+      if (method == "nonce") {
+      } else if (method == "submit") {
+      }
       var req = new XMLHttpRequest();
       req.open('POST', e.payload.url, true);
       req.onload = function(e) {
