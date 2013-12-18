0e44e121e87d9b11d7dd4a137093c7f001e01bee
Ari Wilson <ari.wilson@gmail.com>
2013-12-18 09:38:15 -0800

    delete payload url from request after putting it in httprequest

--- Falldown/src/js/pebble-js-app.js
+++ Falldown/src/js/pebble-js-app.js
@@ -3,9 +3,10 @@
 
 Pebble.addEventListener("appmessage",
     function(e) {
-      var body = JSON.stringify(e.payload);
       var req = new XMLHttpRequest();
       req.open('POST', e.payload.url, true);
+      delete e.payload.url;
+      var body = JSON.stringify(e.payload);
       req.onreadystatechange = function() {
         if (req.readyState == 4 && req.status == 200) {
           Pebble.sendAppMessage(JSON.parse(req.responseText));
