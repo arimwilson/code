3f1f783891d6d847fcda0f9712e5f8be36c44cd7
Ari Wilson <ari.wilson@gmail.com>
2013-12-19 17:41:34 -0800

    add basic configuration page for username

--- Falldown/src/js/pebble-js-app.js
+++ Falldown/src/js/pebble-js-app.js
@@ -2,16 +2,28 @@
 // console.log("log");
 
 Pebble.addEventListener("appmessage",
-    function(e) {
-      var req = new XMLHttpRequest();
-      req.open('POST', e.payload.url, true);
-      delete e.payload.url;
-      var body = JSON.stringify(e.payload);
-      req.onreadystatechange = function() {
-        if (req.readyState == 4 && req.status == 200) {
-          Pebble.sendAppMessage(JSON.parse(req.responseText));
-        }
+  function(e) {
+    var req = new XMLHttpRequest();
+    req.open('POST', e.payload.url, true);
+    delete e.payload.url;
+    var body = JSON.stringify(e.payload);
+    req.onreadystatechange = function() {
+      if (req.readyState == 4 && req.status == 200) {
+        Pebble.sendAppMessage(JSON.parse(req.responseText));
       }
-      req.send(body);
     }
+    req.send(body);
+  }
+);
+
+Pebble.addEventListener("showConfiguration",
+  function(e) {
+    Pebble.openURL("http://pebblescores.appspot.com/configuration.html");
+  }
+);
+
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+    var configuration = JSON.parse(decodeURIComponent(e.response));
+  }
 );
