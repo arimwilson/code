9583ba0c4172b3ff29087445ea8381242cdf2b98
Ari Wilson <ari.wilson@gmail.com>
2014-01-22 01:00:04 -0800

    80 chars

--- BlockFort/pointer_lock_controls.js
+++ BlockFort/pointer_lock_controls.js
@@ -28,13 +28,16 @@ THREE.PointerLockControls = function ( camera ) {
 
     if ( scope.enabled === false ) return;
 
-    var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-    var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    var movementX = event.movementX || event.mozMovementX ||
+                    event.webkitMovementX || 0;
+    var movementY = event.movementY || event.mozMovementY ||
+                    event.webkitMovementY || 0;
 
     yawObject.rotation.y -= movementX * 0.002;
     pitchObject.rotation.x -= movementY * 0.002;
 
-    pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
+    pitchObject.rotation.x = Math.max( - PI_2, Math.min(
+        PI_2, pitchObject.rotation.x ) );
 
   };
 
