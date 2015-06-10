c219ac5994d6e7b4377774d906f67675392150ec
Ari Wilson <ari.wilson@gmail.com>
2015-06-10 01:05:05 -0700

    MyMoncy clone: SQLite schema for currency seller database; basic command line flag parsing

--- Test/market.py
+++ Test/market.py
@@ -1,20 +1,48 @@
-# Try to recreate MyMoncy as a bidding currency seller / user market.
-#
-# "Database"::
-#  Seller 1 ID
-#  Location(s)
-#  Min / max / step amounts
-#  Review(s) (out of 5 stars)
-#  Currency 1 ID / bid / min / max / step amounts
-#  Currency 2 ...
-#
-#  Seller 2 ...
+# Database:
+'''
+create table Seller(
+ Id INTEGER PRIMARY KEY,
+ Contact TEXT
+);
+create table SellerOffer(
+ SellerId INTEGER
+ CurrencySold TEXT,
+ CurrencyBought TEXT,
+ Bid REAL,
+ FOREIGN KEY(SellerId) REFERENCES Seller(Id)
+);
+create table SellerLocation(
+  SellerId INTEGER
+  Location TEXT,
+  FOREIGN KEY(SellerId) REFERENCES Seller(Id)
+);
+create table SellerReview(
+  SellerId INTEGER
+  Rating INTEGER,
+  FOREIGN KEY(SellerId) REFERENCES Seller(Id)
+);
+'''
+# TODO(ariw): Add min/max/step amounts to SellOffer table?
 #
 # Input:
-#  Currency ID
+#  Currency buying
+#  Currency selling
 #  User location
 #  Amount of currency desired
 #
 # Output:
-#  Ordered list of sellers matching location, currency desired sorted by online
-#  auction based on reviews and bid amount.
+#  Ordered list of sellers matching currencies, location, and currency received
+#  sorted by online auction based on reviews and bid amount.
+
+import argparse
+import sqlite3
+
+parser = argparse.ArgumentParser(
+  description = "Bidding currency seller / user market, like MyMoncy.")
+parser.add_argument("currency_id")
+parser.add_argument("user_location")
+parser.add_argument("amount_of_currency")
+parser.add_argument("--database", default="market.db")
+args = parser.parse_args()
+
+conn = sqlite3.connect(args.database)
