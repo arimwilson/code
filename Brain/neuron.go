c562e7d619c401b47ffa1b4ed3e70473a9266f24
Ari Wilson <ari.wilson@gmail.com>
2015-06-29 23:16:13 -0700

    minor fixes neurons

---
+++ Brain/neuron.go
@@ -0,0 +1,49 @@
+package neuron
+
+// ReLU neuron.
+type Neuron struct {
+  Inputs []float64
+  Parameters []float64
+  Gradients []float64
+  Output float64
+}
+
+// n is number of parameters for the neuron.
+func NewNeuron(n int) *Neuron {
+  neuron := new(Neuron)
+  // Initialize parameters randomly from (-0.5, 0.5).
+  for i := 0; i < n; i++ {
+    neuron.Parameters = append(neuron.Parameters, rand.Float64() - 0.5)
+    neuron.Gradients = append(neuron.Gradients, 0.0)
+  }
+  neuron.Output = 0.0
+  return neuron
+}
+
+func (self *Neuron) Forward(inputs []float64) {
+  i := 0
+  self.Inputs = inputs
+  for ; i < len(self.Parameters) - 1; i++ {
+    self.Output += self.Parameters[i] * self.Inputs[i]
+  }
+  self.Output += self.Parameters[i]
+  self.Output = math.Max(0, self.Output)
+}
+
+func (self *Neuron) Backward(pull float64) {
+  if self.Output == 0 {
+   return
+  }
+  i := 0
+  for ; i < len(self.Parameters) - 1; i++ {
+    self.Gradients[i] = self.Inputs[i] * pull - self.Parameters[i]
+  }
+  self.Gradients[i] = pull - self.Parameters[i]
+}
+
+func (self *Neuron) Update() {
+  stepSize := 0.01
+  for i := 0; i < len(self.Parameters); i++ {
+    self.Parameters[i] += stepSize * self.Gradients[i]
+  }
+}
