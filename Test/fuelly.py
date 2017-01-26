7efcadd58837b4536f5071359260a70b30a90595
Ari Wilson <ari.wilson@gmail.com>
2017-01-25 20:01:37 -0800

    initial input for tensorflow fuelly

--- Test/fuelly.py
+++ Test/fuelly.py
@@ -10,30 +10,65 @@ import tensorflow as tf
 
 FLAGS = None
 
-def read():
-  pass
+class Datapoint(object):
+    def __init__(self, header, value_header, row):
+        self.features = {}
+        for i in xrange(len(row)):
+            if header[i] == value_header:
+                self.value = row[i]
+            else:
+                 self.features[header[i]] = row[i]
+
+    def __str__(self):
+        return "Features: %s, value: %s" % (self.features, self.value)
+
+def read_to_datapoints(fuelly_csv_file):
+    reader = csv.reader(open(fuelly_csv_file, 'rb'))
+    datapoints = []
+    header = reader.next()
+    for row in reader:
+        datapoints.append(Datapoint(header, 'mpg', row))
+
+def read(fuelly_csv_file):
+    filename_queue = tf.train.string_input_producer([fuelly_csv_file])
+    reader = tf.TextLineReader()
+    key, value = reader.read(filename_queue)
+    # Format is car name, model, mpg, miles, gallons, price, city percentage
+    # fuelup date, date added, tags, notes, missed_fuelup, partial_fuelup
+    # latitude, longitude, and brand.
+    record_defaults = [
+        [''], [''], [0.0], [0.0], [0.0], [0.0], [0], [''], [''], [''], [''],
+        [0], [0], [0.0], [0.0], ['']]
+    car_name, model, mpg, miles, gallons, price, city_percentage, fuelup_date, \
+        date_added, tags, notes, missed_fuelup, partial_fuelup, latitude, \
+        longitude, brand = tf.decode_csv(value,
+            record_defaults=record_defaults)
+    features = tf.pack([miles, city_percentage, fuelup_date, price])
+    print(features)
 
 def model():
-  pass
+    pass
 
-def train():
-  pass
+def train(model, data):
+    pass
 
-def evaluate():
-  pass
+def evaluate(model, data):
+    pass
 
 def main(_):
-  # Read & parse file into appropriate features.
-  data = read(FLAGS.fuelly_csv_file)
+    # Read & parse file into appropriate features & value.
+    data = read(FLAGS.fuelly_csv_file)
 
-  # Train & eval simple neural network regression model.
-  trained_model = train(model(), data)
-  evaluate(trained_model, data)
+    # Train & eval simple neural network regression model.
+    trained_model = train(model(), data)
+    evaluate(trained_model, data)
+
+    # TODO(ariw): Add capability to test trained model on new examples.
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--fuelly_csv_file', type=str, default='/tmp/fuelups.csv',
-      help='Location of exported Fuelly CSV file')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--fuelly_csv_file', type=str, default='/tmp/fuelups.csv',
+        help='Location of exported Fuelly CSV file')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
