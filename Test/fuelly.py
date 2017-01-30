19a718780da8e9761d97b9978f12dc1fe208d732
Ari Wilson <ari.wilson@gmail.com>
2017-01-29 18:31:56 -0800

    reading data via numpy. regression si hard.

--- Test/fuelly.py
+++ Test/fuelly.py
@@ -3,72 +3,107 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
+import datetime
+import collections
 import csv
 import sys
 
+import numpy as np
 import tensorflow as tf
 
 FLAGS = None
 
-class Datapoint(object):
-    def __init__(self, header, value_header, row):
-        self.features = {}
-        for i in xrange(len(row)):
-            if header[i] == value_header:
-                self.value = row[i]
-            else:
-                 self.features[header[i]] = row[i]
+Dataset = collections.namedtuple('Dataset', ['data', 'target'])
 
-    def __str__(self):
-        return "Features: %s, value: %s" % (self.features, self.value)
+def load_csv_with_header(filename,
+                         target_dtype,
+                         features_dtype,
+                         target_column=-1,
+                         feature_columns=None,
+                         feature_preprocessors=[]):
+  """Load dataset from CSV file with a header row."""
+  with tf.platform.gfile.Open(filename) as csv_file:
+    data_file = csv.reader(csv_file)
+    header = next(data_file)
+    target = []
+    data = []
+    data_file.next()
+    for row in data_file:
+        features = []
+        for j, feature in enumerate(row):
+            k = len(features)
+            if j in feature_columns:
+                if feature_preprocessors[k]:
+                    features.append(feature_preprocessors[k](feature))
+                else:
+                    features.append(feature)
+        data.append(tuple(features))
+        target.append(row.pop(target_column))
+  return Dataset(data=np.asarray(data, dtype=features_dtype),
+                 target=np.asarray(target, dtype=target_dtype))
 
-def read_to_datapoints(fuelly_csv_file):
-    reader = csv.reader(open(fuelly_csv_file, 'rb'))
-    datapoints = []
-    header = reader.next()
-    for row in reader:
-        datapoints.append(Datapoint(header, 'mpg', row))
+def float32_or_none(float_str):
+    try:
+        return np.float32(float_str)
+    except ValueError:
+        return None
+
+# Return number of seconds from date_str to the present.
+def seconds_in_past(date_str):
+    return (datetime.datetime.now() -
+            datetime.datetime.strptime(date_str, "%Y-%m-%d")).total_seconds()
 
 def read(fuelly_csv_file):
-    filename_queue = tf.train.string_input_producer([fuelly_csv_file])
-    reader = tf.TextLineReader()
-    key, value = reader.read(filename_queue)
     # Format is car name, model, mpg, miles, gallons, price, city percentage
-    # fuelup date, date added, tags, notes, missed_fuelup, partial_fuelup
+    # fuelup date, date added, tags, notes, missed fuelup, partial fuelup
     # latitude, longitude, and brand.
-    record_defaults = [
-        [''], [''], [0.0], [0.0], [0.0], [0.0], [0], [''], [''], [''], [''],
-        [0], [0], [0.0], [0.0], ['']]
-    car_name, model, mpg, miles, gallons, price, city_percentage, fuelup_date, \
-        date_added, tags, notes, missed_fuelup, partial_fuelup, latitude, \
-        longitude, brand = tf.decode_csv(value,
-            record_defaults=record_defaults)
-    features = tf.pack([miles, city_percentage, fuelup_date, price])
-    print(features)
+    #
+    # We use miles, price, city percentage, and fuelup date as features and
+    # mpg as target.
+    dataset = load_csv_with_header(
+        fuelly_csv_file, np.float32, np.float32, 2, [3, 5, 6, 7],
+        [None, None, float32_or_none, seconds_in_past])
+    # Fill in missing city percentages with sample mean (MCAR approach).
+    averages = np.nanmean(dataset.data, axis=0)
+    indices = np.where(np.isnan(dataset.data))
+    dataset.data[indices] = np.take(averages, indices[1])
+    return dataset
 
-def model():
-    pass
+def model(dataset):
+    X = tf.placeholder(tf.float32)
+    # Linear model with weight and bias term.
+    W = tf.Variable(np.random.randn())
+    b = tf.Variable(np.random.randn())
+    return X, tf.add(tf.mul(W, X), b)
 
-def train(model, data):
-    pass
+def train(sess, model, dataset):
+    training_step = tf.train.GradientDescentOptimizer(
+        FLAGS.learning_rate).minimize(
+            tf.reduce_mean(tf.square(dataset.target - model[1])))
+    for i in range(10):
+        sess.run(training_step, feed_dict={model[0]: , Y: model[1]})
 
-def evaluate(model, data):
+def evaluate(sess, model, dataset):
     pass
 
 def main(_):
     # Read & parse file into appropriate features & value.
-    data = read(FLAGS.fuelly_csv_file)
+    dataset = read(FLAGS.fuelly_csv_file)
 
-    # Train & eval simple neural network regression model.
-    trained_model = train(model(), data)
-    evaluate(trained_model, data)
-
-    # TODO(ariw): Add capability to test trained model on new examples.
+    # Train & eval model.
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        trained_model = train(sess, model(dataset), dataset)
+        evaluate(sess, trained_model, dataset)
+        # TODO(ariw): Add capability to test trained model on new examples.
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--fuelly_csv_file', type=str, default='/tmp/fuelups.csv',
         help='Location of exported Fuelly CSV file')
+    parser.add_argument(
+        '--learning_rate', type=float, default=0.01,
+        help='Learning rate for gradient descent optimization.')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
