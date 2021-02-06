bfd256f385d5b952be50ac3999c83ffae49876f9
Ari Wilson <ari.wilson@gmail.com>
2021-02-05 16:32:26 -0800

    delete temporaries

--- Test/mempi_rs/target/debug/deps/mempi_rs-a342aada054815ae.d
+++
@@ -1,5 +0,0 @@
-/home/ariw/code/code/Test/mempi_rs/target/debug/deps/mempi_rs-a342aada054815ae: src/main.rs
-
-/home/ariw/code/code/Test/mempi_rs/target/debug/deps/mempi_rs-a342aada054815ae.d: src/main.rs
-
-src/main.rs:
--- Test/mempi_rs/target/debug/mempi_rs.d
+++
@@ -1,1 +0,0 @@
-/home/ariw/code/code/Test/mempi_rs/target/debug/mempi_rs: /home/ariw/code/code/Test/mempi_rs/src/main.rs
